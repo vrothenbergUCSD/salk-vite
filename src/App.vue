bc29f3a5926b8a1d93dfb679e20e44beacf342f7
vrothenbergUCSD <vrothenberg@ucsd.edu>
2022-07-07 11:42:03 -0700

    NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import NavBar from './components/NavBar.vue'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <NavBar/>
   <HelloWorld msg="Hello Vue 3 + Vite" />
 </template>
 
@@ -16,6 +17,6 @@ import HelloWorld from './components/HelloWorld.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
